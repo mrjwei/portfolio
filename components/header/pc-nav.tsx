566c921c5ebc0f5497f2aba47cd27f76f654a32e
Jesse Creatives <jesseweijapan@gmail.com>
2022-02-21 18:12:54 +0900

    Fix header contact button color

--- components/header/pc-nav.tsx
+++ components/header/pc-nav.tsx
@@ -5,15 +5,13 @@ type Props = {
 }
 
 export const PCNav = ({mode}: Props) => {
-  const linkColorClassName = mode === "primary" ? "text-white" : "text-red-300"
-
   return (
     <nav className="hidden lg:block">
       <ul className='flex items-center'>
-        <li><Link href="/"><a className={`px-6 ${linkColorClassName}`}>Top</a></Link></li>
-        <li><Link href="/works"><a className={`px-6 ${linkColorClassName}`}>Works</a></Link></li>
-        <li><Link href="/posts"><a className={`px-6 ${linkColorClassName}`}>Blog</a></Link></li>
-        <li><Link href="/contact"><a className={`button button-white border-2 p-1 px-6 ${linkColorClassName}`}>Contact</a></Link></li>
+        <li><Link href="/"><a className={`px-6 ${mode === "primary" ? "text-white" : "text-red-300"}`}>Top</a></Link></li>
+        <li><Link href="/works"><a className={`px-6 ${mode === "primary" ? "text-white" : "text-red-300"}`}>Works</a></Link></li>
+        <li><Link href="/posts"><a className={`px-6 ${mode === "primary" ? "text-white" : "text-red-300"}`}>Blog</a></Link></li>
+        <li><Link href="/contact"><a className={`button border-2 p-1 px-6 ${mode === "primary" ? "button-white text-white" : "button-primary text-red-300"}`}>Contact</a></Link></li>
       </ul>
     </nav>
   )
