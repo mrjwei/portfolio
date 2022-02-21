8840fb59951e45b4ca916789238e0f76f67b6214
Jesse Creatives <jesseweijapan@gmail.com>
2022-02-21 18:25:37 +0900

    Refactor button classes

--- components/filterButton/index.tsx
+++ components/filterButton/index.tsx
@@ -3,7 +3,7 @@ import {FilterButtonInterface} from '../../types'
 export const FilterButton = ({label, value, isActive, handleClick}: FilterButtonInterface) => {
   return (
     <button
-      className={`button drop-shadow-xl button-bg-white text-sm px-3 py-1 ${isActive && "button-bg-white-reverse"}`}
+      className={`button drop-shadow-xl text-sm px-3 py-1 ${isActive ? "button-bg-white-reverse" : "button-bg-white"}`}
       type="button"
       value={value}
       onClick={handleClick}
