1e008ced92386887ee533fe980bbae6c9ea6084e
Jesse Creatives <jesseweijapan@gmail.com>
2022-03-02 22:36:22 +0900

    Fix article styles on mobile

--- components/article/index.tsx
+++ components/article/index.tsx
@@ -13,16 +13,16 @@ export const Article = ({entry, prevData, nextData}: Props) => {
   const {title, tags, content} = entry
 
   return "image" in entry ? (
-    <article className='bg-white p-20 rounded-md lg:col-span-2'>
+    <article className='bg-white p-8 lg:p-20 rounded-md lg:col-span-2'>
       <div>
-        <h1 className="text-5xl mb-3">{title}</h1>
+        <h1 className="text-3xl lg:text-5xl mb-3">{title}</h1>
         <Tags tags={tags} isLink={true} fontSize="text-lg" />
-        <div className='mt-12 mb-24' dangerouslySetInnerHTML={{
+        <div className='mt-6 mb-12' dangerouslySetInnerHTML={{
           __html: `<div class="prose prose-a:text-red-300 prose-h2:text-3xl prose-h2:mb-8 prose-h3:text-2xl prose-h3:mb-6 prose-h4:text-xl prose-h4:underline prose-h4:mb-6 prose-pre:bg-transparent prose-pre:p-0 prose-">${content}</div>`
         }} />
       </div>
-      <div className='w-full flex justify-between'>
-        <div className='flex'>
+      <div className='w-full lg:flex lg:justify-between'>
+        <div className='flex justify-between mb-6 lg:mb-0'>
           {prevData ? (
             <Button mode='primary-text' className='mr-12'>
               <Link href={`/works/${prevData.id}`}>
@@ -47,24 +47,24 @@ export const Article = ({entry, prevData, nextData}: Props) => {
           )}
         </div>
         <div>
-          <Button mode="primary-border">
+          <Button mode="primary-border" className='mx-auto lg:mx-0'>
             <Link href="/works">Back to Works</Link>
           </Button>
         </div>
       </div>
     </article>
   ) : (
-    <article className='bg-white p-20 rounded-md lg:col-span-2'>
+    <article className='bg-white p-8 lg:p-20 rounded-md lg:col-span-2'>
       <div>
         <time className='block mb-6 text-gray-400'>{entry.date}</time>
-        <h1 className="text-5xl mb-3">{title}</h1>
+        <h1 className="text-3xl lg:text-5xl mb-3">{title}</h1>
         <Tags tags={tags} isLink={true} fontSize="text-lg" />
         <div className='mt-12 mb-24' dangerouslySetInnerHTML={{
           __html: `<div class="prose prose-a:text-red-300 prose-h2:text-3xl prose-h2:mb-8 prose-h3:text-2xl prose-h3:mb-6 prose-h4:text-xl prose-h4:underline prose-h4:mb-6 prose-pre:bg-transparent prose-pre:p-0 prose-">${content}</div>`
         }} />
       </div>
-      <div className='w-full flex justify-between'>
-        <div className='flex'>
+      <div className='w-full lg:flex lg:justify-between'>
+        <div className='flex justify-between mb-6 lg:mb-0'>
           {prevData ? (
             <Button mode='primary-text' className='mr-12'>
               <Link href={`/posts/${prevData.id}`}>
@@ -89,7 +89,7 @@ export const Article = ({entry, prevData, nextData}: Props) => {
           )}
         </div>
         <div>
-          <Button mode="primary-border">
+          <Button mode="primary-border" className='mx-auto lg:mx-0'>
             <Link href="/posts">Back to Posts</Link>
           </Button>
         </div>
