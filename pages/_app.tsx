11b5c4c5632d81c827e7385e689513737c46d313
Jesse Creatives <jesseweijapan@gmail.com>
2022-04-02 22:58:07 +0900

    Add seo tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import 'highlight.js/styles/vs2015.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id="analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
