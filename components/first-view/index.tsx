4b8f821c256635613b75f420ac342103766885dc
Jesse Creatives <jesseweijapan@gmail.com>
2022-04-12 13:43:47 +0900

    style: fine-tune responsiveness of first view component and set line height for headings

--- components/first-view/index.tsx
+++ components/first-view/index.tsx
@@ -9,7 +9,7 @@ import {
 type WidthType = number | null
 
 export const FirstView = () => {
-  const {t} = useTranslation("top")
+  const {t, lang} = useTranslation("top")
 
   const [width, setWidth] = useState(null as WidthType)
 
@@ -31,23 +31,23 @@ export const FirstView = () => {
   }, [])
 
   return (
-    <div className="w-full pt-28 bg-red-300 md:h-[35rem] lg:h-screen lg:pt-36">
+    <div className="w-full pt-28 bg-red-300 xl:h-screen xl:pt-36">
       <div className="container h-full md:grid md:grid-cols-2 md:grid-rows-1">
-        <div className="mb-16 md:mb-0 md:mr-14 md:flex md:flex-col md:justify-start lg:translate-y-10">
+        <div className="mb-16 md:mb-0 md:mr-14 md:pb-36 md:flex md:flex-col md:justify-start lg:translate-y-10">
           <h1 className='text-white mb-12'>
             {t("title.part1")}<br />
             {t("title.part2")}
           </h1>
           <div className='md:flex'>
             <Link href="/contact">
-              <a className='flex justify-center items-center button button-bg-white mb-8 md:mb-0 md:max-w-[12rem] md:mr-12'><span className='mr-3'>{t("contactButton")}</span> <BsArrowRight /></a>
+              <a className='flex justify-center items-center button button-bg-white mb-8 md:mb-0 md:max-w-[12rem] md:mr-8 xl:mr-12'><span className='mr-2'>{t("contactButton")}</span> <BsArrowRight /></a>
             </Link>
             <Link href="/works">
               <a className='flex justify-center items-center button button-white md:max-w-[12rem]'><span className='mr-3'>{t("seeWorksButton")}</span> <BsArrowRight /></a>
             </Link>
           </div>
         </div>
-        <div className="w-full h-[30rem] relative flex  md:h-full">
+        <div className="w-full min-h-[30rem] relative flex">
           <Image src="/images/global-images/first-view-illustration.svg" alt="first view illustration of Jesse Wei" className="" layout='fill' objectFit='contain' objectPosition={`${(width && width >= 768) ? "right bottom" : "center bottom"}`} />
         </div>
       </div>
